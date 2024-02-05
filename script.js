37d76a52b25e4b00afbbe967d1323613e2ad5f56
sumit kumar <72662152+sumit-singh0044@users.noreply.github.com>
2024-02-05 11:34:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ window.onload = function() {
 };
 
 let image=document.getElementById('img');
-let images=['/images/pic1.jpg','/images/pic2.jpg','/images/pic3.jpg','/images/pic4.jpg','/images/pic5.jpg','/images/pic6.jpg','/images/pic7.jpg','/images/pic8.jpg'];
+let images=['images/pic1.jpg','images/pic2.jpg','images/pic3.jpg','images/pic4.jpg','images/pic5.jpg','images/pic6.jpg','images/pic7.jpg','images/pic8.jpg'];
 setInterval(function()
 {
   let random=Math.floor(Math.random()*8);
